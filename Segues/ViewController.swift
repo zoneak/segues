cbf3a9b832f977689020df96d882da2f8101eb43
Adriano Kaito <adrianokaito@Adrianos-MacBook-Pro.local>
2018-12-03 21:44:14 -0200

    commit

---
+++ Segues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by Adriano Kaito on 26/11/18.
+//  Copyright © 2018 AK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
